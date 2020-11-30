533ac7658bf08a9b6fbfced6607874eb71801dbd
Guillaume DUFOUR <46460244+Guillaume-Dufour@users.noreply.github.com>
2020-11-30 16:53:57 +0100

    ControllerLogin modif

--- src/main/java/business/controllers/LoginController.java
+++ src/main/java/business/controllers/LoginController.java
@@ -2,10 +2,18 @@ package business.controllers;
 
 import business.facades.UserFacade;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
     UserFacade userFacade;
+
+    @FXML
+    TextField mailField;
+
+    @FXML
+    TextField passwordField;
 
     public void login(ActionEvent event) {
 
