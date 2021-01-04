9279106cd5ead89348c22e37a7c91f074e238126
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-01-04 22:59:08 +0100

    fix pull

--- src/main/java/cooptool/business/controllers/mentoringDemandManagement/MentoringDemandController.java
+++ src/main/java/cooptool/business/controllers/mentoringDemandManagement/MentoringDemandController.java
@@ -31,8 +31,7 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MentoringDemandController implements Initializable {
-    @FXML
-    Pane header_admin,header_student;
+
     @FXML
     Text descriptionArea;
     @FXML
@@ -56,10 +55,8 @@ public class MentoringDemandController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         if (userFacade.isCurrentUserStudent()){
-            header_admin.setVisible(false);
-
+            //TODO: changer la condition
         } else {
-            header_student.setVisible(false);
             disableStudentRights();
         }
 
