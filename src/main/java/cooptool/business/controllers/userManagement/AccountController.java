ac2df92a9e32f23fd56a792853255b314ffbddd3
Guillaume DUFOUR <46460244+Guillaume-Dufour@users.noreply.github.com>
2021-01-06 13:09:16 +0100

    Optimize imports

--- src/main/java/cooptool/business/controllers/userManagement/AccountController.java
+++ src/main/java/cooptool/business/controllers/userManagement/AccountController.java
@@ -5,14 +5,12 @@ import cooptool.business.ViewPath;
 import cooptool.business.facades.UserFacade;
 import cooptool.models.objects.StudentRole;
 import cooptool.models.objects.User;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 import java.net.URL;
